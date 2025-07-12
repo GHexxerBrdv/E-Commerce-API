22da1398d2a84f3e23f95799509584f8f97134d3
GaurangBrdv <gaurang1415@gmail.com>
2025-07-12 19:41:55 +0530

    product model

---
+++ model/productModel.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const productModel = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        maxLength: 8
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        lowecase: true,
+        enum: ["electronics", "clothing", "books", "accessories", "food", "furniture", "others"]
+    },
+    brand: {
+        type: String,
+        requried: true
+    },
+    stock: {
+        type: Number,
+        required: true,
+        maxLength: 4,
+        default: 1
+    },
+    images: [
+        {
+            public_id: {
+                type: String
+            },
+            url: {
+                type: String
+            }
+        }
+
+    ],
+    rating: {
+        type: Number,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Users"
+            },
+            name: String,
+            rating: Number,
+            comment: String,
+            createdAt: {
+                type: Date,
+                default: Date.now()
+            }
+        }
+    ],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model("Products", productModel);
